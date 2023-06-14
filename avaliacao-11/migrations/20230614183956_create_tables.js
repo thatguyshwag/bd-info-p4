dc91db0ab4b98d41479b4fd4114df7e4984f3599
Fabio Barros <112815722+thatguyshwag@users.noreply.github.com>
2023-06-14 16:57:31 -0300

    Add files via upload

---
+++ avaliacao-11/migrations/20230614183956_create_tables.js
@@ -0,0 +1,26 @@
+exports.up = function(knex) {
+    return knex.schema
+      .createTable('alunos', function(table) {
+        table.increments('id');
+        table.string('nome').notNullable();
+        table.string('email').notNullable().unique();
+      })
+      .createTable('professores', function(table) {
+        table.increments('id');
+        table.string('nome').notNullable();
+        table.string('email').notNullable().unique();
+      })
+      .createTable('disciplinas', function(table) {
+        table.increments('id');
+        table.string('nome').notNullable();
+        table.integer('professor_id').unsigned();
+        table.foreign('professor_id').references('professores.id');
+      });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema
+      .dropTableIfExists('disciplinas')
+      .dropTableIfExists('professores')
+      .dropTableIfExists('alunos');
+  };
